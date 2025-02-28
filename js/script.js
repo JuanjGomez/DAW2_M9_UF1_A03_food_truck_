03bc125b02ca55bc697a3aa942980733c562906e
kilianruiz <451093.joan23@fje.edu>
2025-02-28 16:52:34 +0100

    nav // dropdown // btn login y register

--- js/script.js
+++ js/script.js
@@ -21,3 +21,18 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     
 }); 
+
+//funcion de js de tabs index de los platos
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
